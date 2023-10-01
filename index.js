5e2b969cbec5c1d9a1ae39044c21b5712991d718
SilK <knightsoumya207@gmail.com>
2023-10-02 00:35:43 +0530

    func
    added functionality to the buttons

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-let one = document.getElementById("one")
-let two = document.getElementById("two")
-let three = document.getElementById("three")
+let one = document.getElementById("plus")
+let two = document.getElementById("p")
+let count = 0
 
-console.log(one)
+function plusone() {
+    count += 1
+    one.textContent = count
+}
+
+function plustwo() {
+    count += 2
+    one.textContent = count
+}
+
+function plusthree() {
+    count += 3
+    one.textContent = count
+}
+
+function pone() {
+    count += 1
+    two.textContent = count
+}
+
+function ptwo() {
+    count += 2
+    twoo.textContent = count
+}
+
+function pthree() {
+    count += 3
+    two.textContent = count
+}
